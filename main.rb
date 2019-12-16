f83cbbc2acb8997d76b336f9f60fe96effa48436
Maxime Dupuis <webf0x@hotmail.com>
2019-12-15 19:13:06 -0500

    level 1

--- main.rb
+++ main.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
-puts 'Hello world!'
+file_content = File.open('input.sql').read
+masses = file_content.split("\n").map(&:to_i)
+fuels = masses.map { |mass| mass / 3 - 2 }
+solution = fuels.reduce(0, :+)
+puts solution
