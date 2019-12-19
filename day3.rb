1aa61a7677867297f663aee3623eebdd9a06eaf1
Maxime Dupuis <webf0x@hotmail.com>
2019-12-18 22:50:44 -0500

    Point and manathan distance

--- day3.rb
+++ day3.rb
@@ -1,17 +1,27 @@
+require "test/unit/assertions"
+include Test::Unit::Assertions
+
 #max
 def manathan_distance(point1, point2)
-  return 0
+  dx = (point1.x - point2.x).abs
+  dy = (point1.y - point2.y).abs
+  dx + dy
 end
 
 #max
 class Point
   attr_accessor :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
 end
 
 #phil
 class Segment
   def is_collision?(segment)
-    si meme sens return false
+    #si meme sens return false
   end
 end
 
@@ -24,13 +34,44 @@ end
 
 #bix
 def init_wires()
-  input => 2 Wire
+  #input => 2 Wire
 end
 
 #bix
 def main
-  for fil 1, each segment
-    check if fil 2 collides  
+  #for fil 1, each segment
+    #check if fil 2 collides  
 end
 
-main()
+def test_point
+  point = Point.new(2, 3)
+  assert_equal 2, point.x
+  assert_equal 3, point.y
+end
+
+def test_manathan_distance
+  assert_equal 0, manathan_distance(Point.new(0,0), Point.new(0,0))
+
+  assert_equal 3, manathan_distance(Point.new(0,0), Point.new(0,3))
+  assert_equal 3, manathan_distance(Point.new(0,0), Point.new(3,0))
+  assert_equal 3, manathan_distance(Point.new(3,0), Point.new(0,0))
+  assert_equal 3, manathan_distance(Point.new(0,3), Point.new(0,0))
+
+  assert_equal 3, manathan_distance(Point.new(0,0), Point.new(0,-3))
+  assert_equal 3, manathan_distance(Point.new(0,0), Point.new(-3,0))
+  assert_equal 3, manathan_distance(Point.new(-3,0), Point.new(0,0))
+  assert_equal 3, manathan_distance(Point.new(0,-3), Point.new(0,0))
+
+  assert_equal 3, manathan_distance(Point.new(0,0), Point.new(1,2))
+  assert_equal 3, manathan_distance(Point.new(0,0), Point.new(2,1))
+  assert_equal 3, manathan_distance(Point.new(1,2), Point.new(0,0))
+  assert_equal 3, manathan_distance(Point.new(2,1), Point.new(0,0))
+end
+
+def all_tests
+  test_manathan_distance
+  test_point
+end
+
+all_tests
+main
