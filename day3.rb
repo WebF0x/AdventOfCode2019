1e493d003ddb2e4cef0fe09dd3fe12e076e47241
Maxime Dupuis <webf0x@hotmail.com>
2019-12-18 22:36:14 -0500

    init day3 part 1

---
+++ day3.rb
@@ -0,0 +1,36 @@
+#max
+def manathan_distance(point1, point2)
+  return 0
+end
+
+#max
+class Point
+  attr_accessor :x, :y
+end
+
+#phil
+class Segment
+  def is_collision?(segment)
+    si meme sens return false
+  end
+end
+
+#phil
+class Wire
+  def next_segment()
+
+  end
+end
+
+#bix
+def init_wires()
+  input => 2 Wire
+end
+
+#bix
+def main
+  for fil 1, each segment
+    check if fil 2 collides  
+end
+
+main()
